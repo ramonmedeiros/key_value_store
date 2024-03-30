fe5c91eb010ecc96b9b9cf37e044719a3c93e9f6
Ramon Medeiros <ramon.rnm@gmail.com>
2024-03-30 10:38:44 +0100

    add tests to verify distribution

--- internal/keystore/hash_ring_test.go
+++ internal/keystore/hash_ring_test.go
@@ -10,31 +10,65 @@ import (
 
 	"github.com/ramonmedeiros/key_value_store/internal/hash"
 	"github.com/stretchr/testify/require"
+	"gonum.org/v1/gonum/stat"
 )
 
 // TestDistributionNodes prints amount of items per node
 func TestDistributionNodes(t *testing.T) {
-	expireTime = time.Second * 5
-	keyStore, err := New(slog.New(slog.NewJSONHandler(os.Stderr, nil)), hash.New(), 5, 10)
-	require.NoError(t, err)
-
-	totalKeys := 100000
-
-	for index := 0; index < totalKeys; index++ {
-		key := "key_" + strconv.Itoa(index)
-		value := []byte("value")
-		err := keyStore.AddKey(key, value)
-		require.NoError(t, err)
+	testcases := []struct {
+		name         string
+		nodes        int
+		virtualNodes int
+		totalKeys    int
+	}{
+		{
+			name:         "single node",
+			nodes:        1,
+			virtualNodes: 1,
+			totalKeys:    100000,
+		},
+		{
+			name:         "10 node, 1000 virtual",
+			nodes:        10,
+			virtualNodes: 1000,
+			totalKeys:    1000000,
+		},
 	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			expireTime = time.Second * 5
+			keyStore, err := New(slog.New(slog.NewJSONHandler(os.Stderr, nil)), hash.New(), tc.nodes, tc.virtualNodes)
+			require.NoError(t, err)
 
-	for _, node := range keyStore.nodes {
-		fmt.Printf("%d items\n", len(node.cache))
-	}
+			beforeAdd := time.Now()
+			for index := 0; index < tc.totalKeys; index++ {
+				key := "key_" + strconv.Itoa(index)
+				value := []byte("value")
+				err := keyStore.AddKey(key, value)
+				require.NoError(t, err)
+			}
+			fmt.Printf("adding keys %s\n", time.Since(beforeAdd))
 
-	for index := 0; index < totalKeys; index++ {
-		key := "key_" + strconv.Itoa(index)
-		value, err := keyStore.GetKey(key)
-		require.NoError(t, err)
-		require.Equal(t, "value", string(value))
+			beforeCalc := time.Now()
+			pop := []float64{}
+			weights := []float64{}
+			for _, n := range keyStore.sortedNodes {
+				pop = append(pop, float64(n))
+				weights = append(weights, float64(len(keyStore.nodes[n].cache)))
+			}
+
+			standardDeviation := stat.StdDev(pop, weights)
+			fmt.Printf("%s - The weighted standard deviation of the samples is %.4f\n", tc.name, standardDeviation)
+			fmt.Printf("deviation %s\n", time.Since(beforeCalc))
+
+			beforeGet := time.Now()
+			for index := 0; index < tc.totalKeys; index++ {
+				key := "key_" + strconv.Itoa(index)
+				value, err := keyStore.GetKey(key)
+				require.NoError(t, err)
+				require.Equal(t, "value", string(value))
+			}
+			fmt.Printf("getting keys %s\n", time.Since(beforeGet))
+		})
 	}
 }
